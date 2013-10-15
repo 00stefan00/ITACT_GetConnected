d653dac8b43affbbeff7052a5f6604cf433de808
Stefan <stefan.strijker@outlook.com>
2013-10-15 16:19:23 +0200

    updated marketplace screens

---
+++ src/com/example/getconnected/CreateRideActivity.java
@@ -0,0 +1,16 @@
+package com.example.getconnected;
+
+import android.os.Bundle;
+
+public class CreateRideActivity extends BaseActivity{
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+
+	setContentView(R.layout.activity_createride);
+	
+	
+	}
+}
--- src/com/example/getconnected/MarketplaceActivity.java
+++ src/com/example/getconnected/MarketplaceActivity.java
@@ -10,6 +10,7 @@ public class MarketplaceActivity extends BaseActivity{
 
 	private Button request_ride_btn;
 	private Button display_requests_btn;
+	private Button create_ride_btn;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,17 @@ public class MarketplaceActivity extends BaseActivity{
 		
 		@Override
 		public void onClick(View v) {
-			Intent intent = new Intent(MarketplaceActivity.this, OfferRideActivity.class);
+			Intent intent = new Intent(MarketplaceActivity.this, RequestRideActivity.class);
+			startActivityForResult(intent, 1);
+		}
+	});
+	
+	create_ride_btn = (Button) findViewById(R.id.create_ride_btn);
+	create_ride_btn.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(MarketplaceActivity.this, CreateRideActivity.class);
 			startActivityForResult(intent, 1);
 		}
 	});
