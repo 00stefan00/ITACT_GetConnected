f366d37bbf714303d1fe80aa7f00db622270aaf3
NicovPelt <nicobn@live.nl>
2013-10-16 09:35:22 +0200

    Map added

--- src/com/example/getconnected/MapActivity.java
+++ src/com/example/getconnected/MapActivity.java
@@ -1,5 +1,10 @@
 package com.example.getconnected;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,8 +13,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MapActivity extends BaseActivity {
-
+public class MapActivity extends Activity {//BaseActivity {
+/*
 	private Button buttonGetLocation;
 	private TextView textLocation;
 	protected GPSLocator locator;
@@ -18,7 +23,6 @@ public class MapActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		initLayout(R.string.title_activity_main, true, true, true, false);
 		
 		locator = new GPSLocator(getApplicationContext());
 		
@@ -39,5 +43,24 @@ public class MapActivity extends BaseActivity {
 		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
 	}
+*/
+	private MapController mapController;
+    private MapView mapView;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        mapController = mapView.getController();
+        mapController.setZoom(15);
+        GeoPoint point2 = new GeoPoint(51496994, -134733);
+        mapController.setCenter(point2);
+    }
+    protected boolean isRouteDisplayed() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 }
