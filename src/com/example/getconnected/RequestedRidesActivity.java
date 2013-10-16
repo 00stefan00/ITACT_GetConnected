b03cf7105848c73da40870b7bf42e32186bd459f
Stefan <stefan.strijker@outlook.com>
2013-10-16 12:53:30 +0200

    Did stuff that fixed other stuff

--- src/com/example/getconnected/RequestedRidesActivity.java
+++ src/com/example/getconnected/RequestedRidesActivity.java
@@ -1,13 +1,23 @@
 package com.example.getconnected;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
 
-public class RequestedRidesActivity extends BaseActivity{
+import com.util.getconnected.ActivityHelper.DatePickerFragment;
+
+public class RequestedRidesActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_requestedrides);
+
+	}
+
+	public void showDatePickerDialog(View v) {
+		DialogFragment newFragment = new DatePickerFragment();
+		newFragment.show(getFragmentManager(), "datePicker");
 	}
 
 }
