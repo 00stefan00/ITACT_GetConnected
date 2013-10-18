9186c2363d41dc66fa91b71197742d7e99592539
NicovPelt <nicobn@live.nl>
2013-10-18 11:00:44 +0200

    Improved readability of MapActivity

--- src/com/app/getconnected/activities/MapActivity.java
+++ src/com/app/getconnected/activities/MapActivity.java
@@ -2,11 +2,8 @@ package com.app.getconnected.activities;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.app.getconnected.R;
 import com.app.getconnected.gps.GPSLocator;
-
-
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
@@ -14,11 +11,17 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.MyLocationOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
-
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
+
+/**
+ * 
+ * @author Nico
+ * 
+ * 
+ */
 public class MapActivity extends BaseActivity {
 
 	private MapController mapController;
@@ -32,7 +35,26 @@ public class MapActivity extends BaseActivity {
         setContentView(R.layout.activity_map);
         initLayout(R.string.title_activity_map, true, true, true, false);
         
-        mapView = (MapView) findViewById(R.id.mapview);
+        createMap();
+        addLocationOverlay();
+        
+        ArrayList<OverlayItem> overlayItemArray = new ArrayList<OverlayItem>();
+        OverlayItem olItem = new OverlayItem("Bus Stop", "Groningen, Zernike", new GeoPoint(53.241, 6.534));
+        OverlayItem olItem2 = new OverlayItem("niet Bus Stop", "iets anders", new GeoPoint(53.240, 6.534));
+        overlayItemArray.add(olItem);
+        overlayItemArray.add(olItem2);
+        MyOwnItemizedOverlay overlay = new MyOwnItemizedOverlay(this, overlayItemArray);
+        mapView.getOverlays().add(overlay);
+    }
+    
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.map, menu);
+		return true;
+	}
+    
+    private void createMap(){
+    	mapView = (MapView) findViewById(R.id.mapview);
         mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setBuiltInZoomControls(true);
         mapController = mapView.getController();
@@ -40,8 +62,10 @@ public class MapActivity extends BaseActivity {
         locator = new GPSLocator(getApplicationContext());
         GeoPoint point2 = new GeoPoint(locator.getLatitude(), locator.getLongitude());
         mapController.setCenter(point2);
-        
-        myLocationoverlay = new MyLocationOverlay(this, mapView);
+    }
+    
+    private void addLocationOverlay(){
+    	myLocationoverlay = new MyLocationOverlay(this, mapView);
         myLocationoverlay.disableCompass();
         myLocationoverlay.disableFollowLocation();
         myLocationoverlay.setDrawAccuracyEnabled(true);
@@ -53,24 +77,8 @@ public class MapActivity extends BaseActivity {
         });
         
         myLocationoverlay.enableMyLocation();
-        
-        ArrayList<OverlayItem> overlayItemArray = new ArrayList<OverlayItem>();
-        OverlayItem olItem = new OverlayItem("Bus Stop", "Groningen, Zernike", new GeoPoint(53.241, 6.534));
-        OverlayItem olItem2 = new OverlayItem("niet Bus Stop", "iets anders", new GeoPoint(53.240, 6.534));
-        
-        overlayItemArray.add(olItem);
-        overlayItemArray.add(olItem2);
-        MyOwnItemizedOverlay overlay = new MyOwnItemizedOverlay(this, overlayItemArray);
-        mapView.getOverlays().add(overlay);
         mapView.getOverlays().add(myLocationoverlay);
-    	
     }
-    
-    @Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.map, menu);
-		return true;
-	}
     
     public class MyOwnItemizedOverlay extends ItemizedIconOverlay<OverlayItem> {
         protected Context mContext;
