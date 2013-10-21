d05d57748b23d6e739b278a32884c8ff92075e16
啊Denko啊 <juss4dabanta@gmail.com>
2013-10-21 17:08:32 +0200

    Added simple dummy data representing api for registration/login

--- src/com/app/getconnected/activities/LoginActivity.java
+++ src/com/app/getconnected/activities/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.app.getconnected.activities;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import com.app.getconnected.R;
+import com.app.getconnected.rest.RESTRequest;
+import com.app.getconnected.rest.RESTRequest.Method;
 import com.exception.getconnected.FieldValidationException;
 import com.util.getconnected.FieldValidator;
+import com.util.getconnected.JSONParser;
 
 public class LoginActivity extends BaseActivity {
 	
@@ -47,9 +56,49 @@ public class LoginActivity extends BaseActivity {
 		}
 		if(validInput)
 		{
-			loggedIn=true;
-			Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-			startActivityForResult(intent, 1);
+			if(attemptApiLogin())
+			{
+				loggedIn=true;
+				Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+				startActivityForResult(intent, 1);
+			}
+			else
+			{
+				fieldUsername.setError(getString(R.string.login_incorrect_credentials));
+			}
 		}
+	}
+	private boolean attemptApiLogin()
+	{
+		String username=fieldUsername.getText().toString();
+		String password=fieldPassword.getText().toString();		
+		Map<String, String> hashMap = new HashMap<String,String>();
+		hashMap.put("username", username);
+		hashMap.put("password", password);
+		//TODO put api base somewhere central/logical.
+		String apiBase="http://127.0.0.1/OpenRideServer-RS/resources/";
+		//TODO find out the proper login url
+		String url=apiBase+"user/login";
+		RESTRequest request = new RESTRequest(url);
+		request.setMethod(Method.POST);
+		String result="";
+		try {
+			JSONObject jObj=JSONParser.getInstance().parseMapAsObject(hashMap);
+			String jsonString="{\"LoginRequest\":"+jObj.toString()+"}";
+			request.putString("json", jsonString);
+			//TODO implement once API is reachable
+			//result = request.execute().get();
+			if(username.equals("user"))
+				result="200 OK";
+			else
+				result="Unknown";
+		/*} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		} catch (ExecutionException e1) {
+			e1.printStackTrace();*/
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return result.equals("200 OK");
 	}
 }
--- src/com/app/getconnected/activities/RegisterActivity.java
+++ src/com/app/getconnected/activities/RegisterActivity.java
@@ -1,14 +1,25 @@
 package com.app.getconnected.activities;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.app.getconnected.R;
+import com.app.getconnected.rest.RESTRequest;
+import com.app.getconnected.rest.RESTRequest.Method;
 import com.exception.getconnected.FieldValidationException;
 import com.util.getconnected.FieldValidator;
+import com.util.getconnected.JSONParser;
 
 public class RegisterActivity extends BaseActivity {
 	
@@ -58,8 +69,58 @@ public class RegisterActivity extends BaseActivity {
 		}
 		if(validInput)
 		{
-		Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-		startActivityForResult(intent, 1);
+			if(attemptApiRegister())
+			{
+				Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+				startActivityForResult(intent, 1);
+			}
+			else
+			{
+				fieldUsername.setError(getString(R.string.registration_failed));
+			}
 		}
+	}
+	private boolean attemptApiRegister()
+	{
+		String username=fieldUsername.getText().toString();
+		String password=fieldPassword.getText().toString();
+		String firstName=fieldFirstName.getText().toString();
+		String lastName=fieldLastName.getText().toString();
+		String telephoneNumber=fieldTelephoneNumber.getText().toString();
+		String email=fieldEmail.getText().toString();
+		String gender=((RadioButton)findViewById(fieldGender.getCheckedRadioButtonId())).getTag().toString();
+		
+		Map<String, String> hashMap = new HashMap<String,String>();
+		hashMap.put("username", username);
+		hashMap.put("randomPass", password);
+		hashMap.put("firstname", firstName);
+		hashMap.put("lastname", lastName);
+		hashMap.put("telephonenumber", telephoneNumber);
+		hashMap.put("email", email);
+		hashMap.put("gender", gender);
+		
+		//TODO put api base somewhere central/logical.
+		String apiBase="http://127.0.0.1/OpenRideServer-RS/resources/";
+		String url=apiBase+"register";
+		RESTRequest request = new RESTRequest(url);
+		request.setMethod(Method.POST);
+		String result="";
+		//{"RegisterRequest":{"username":"pdevos","randomPass":"randomPass","firstname":"Peter","lastname":"de Vos","gender":"M","email":"example@itract-project.eu","mobilePhoneNumber":"0612345678"}}
+		try {
+			JSONObject jObj=JSONParser.getInstance().parseMapAsObject(hashMap);
+			String jsonString="{\"RegisterRequest\":"+jObj.toString()+"}";
+			Log.d("DEBUG", jsonString);
+			request.putString("json", jsonString);
+			//TODO implement once API is reachable
+			//result = request.execute().get();
+			result="201 Created";
+		/*} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		} catch (ExecutionException e1) {
+			e1.printStackTrace();*/
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return result.equals("201 Created");
 	}
 }
