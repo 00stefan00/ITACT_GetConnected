8729efa78f34be8a83a832b81463f106dfcb8eb2
Stefan Boonstra <stefanboonstra92@gmail.com>
2013-10-21 12:36:38 +0200

    Removed unwanted intent from MainActivity
    Don't push these kind of things to the shared branch :)

--- src/com/app/getconnected/activities/MainActivity.java
+++ src/com/app/getconnected/activities/MainActivity.java
@@ -13,10 +13,6 @@ public class MainActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initLayout(R.string.title_activity_main, false, false, false, false);
-		
-		Intent intent = new Intent(this, BusStopDetailsActivity.class);
-		intent.putExtra("id", 114787);
-		startActivity(intent);
 	}
 
 	@Override
