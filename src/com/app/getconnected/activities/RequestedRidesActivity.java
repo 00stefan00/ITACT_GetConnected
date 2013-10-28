0438684259b47113b6de9b76858c82ebbeb11b41
Stefan <stefan.strijker@outlook.com>
2013-10-28 12:54:40 +0100

    before show

--- src/com/app/getconnected/activities/RequestedRidesActivity.java
+++ src/com/app/getconnected/activities/RequestedRidesActivity.java
@@ -1,25 +1,97 @@
 package com.app.getconnected.activities;
 
+import java.util.concurrent.TimeUnit;
+
 import com.app.getconnected.R;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableLayout.LayoutParams;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.util.getconnected.ActivityHelper.DatePickerFragment;
 
 public class RequestedRidesActivity extends BaseActivity {
+	TableLayout tl;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_requestedrides);
 		initLayout(R.string.title_activity_requestrides, true, true, true, false);
+		
+		tableinit();
+		addTableRow("08:30", "12:00", "Groningen", "Berlijn");
 	}
+
+	private void tableinit() {
+		tl = (TableLayout) findViewById(R.id.tableLayoutRequestedRides);
+		
+		TextView departure_time = new TextView(this);
+		departure_time.setText(getResources().getString(R.string.departure_time));
+        TextView arrival_time = new TextView(this);
+        arrival_time.setText(getResources().getString(R.string.arrival_time));
+        TextView from = new TextView(this);
+        from.setText(getResources().getString(R.string.location_from));
+        TextView destination = new TextView(this);
+        destination.setText(getResources().getString(R.string.location_destination));
+        
+        TableRow rowHeader = new TableRow(this);
+
+        rowHeader.addView(departure_time);
+        rowHeader.addView(arrival_time);
+        rowHeader.addView(from);
+        rowHeader.addView(destination);   
+        
+        tl.addView(rowHeader, new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        addTableRow("", "", "", "");
+        tl.setStretchAllColumns(true);		
+	}
+	
+	private void addTableRow(String departurev, String arrivalv, String fromv, String destinationv){
+		TextView departure_time = new TextView(this);
+		departure_time.setText(departurev);
+        TextView arrival_time = new TextView(this);
+        arrival_time.setText(arrivalv);
+        TextView from = new TextView(this);
+        from.setText(fromv);
+        TextView destination = new TextView(this);
+        destination.setText(destinationv);
+        
+        TableRow datarow = new TableRow(this);
+
+        datarow.addView(departure_time);
+        datarow.addView(arrival_time);
+        datarow.addView(from);
+        datarow.addView(destination);   
+        
+        tl.addView(datarow, new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		
+        tl.setStretchAllColumns(true);		
+	}
+	
 
 	public void showDatePickerDialog(View v) {
 		DialogFragment newFragment = new DatePickerFragment();
 		newFragment.show(getFragmentManager(), "datePicker");
+		
+		if(DatePickerFragment.year == 2014){
+			tl.removeAllViewsInLayout();
+			tableinit();
+		}
+		if(DatePickerFragment.year == 2013){
+			tl.removeAllViewsInLayout();
+			tableinit();
+			addTableRow("08:30", "12:00", "Groningen", "Berlijn");
+		}
+		tl.invalidate();
+	    tl.refreshDrawableState();	
 	}
+	
+	
+	    
 
 }
