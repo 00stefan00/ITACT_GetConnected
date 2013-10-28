f9cb095edef093e72efb33b0e36a3612793ae466
Stefan Boonstra <stefanboonstra@hotmail.com>
2013-10-28 14:10:29 +0100

    Updated IP-adresses

--- src/com/app/getconnected/activities/MapActivity.java
+++ src/com/app/getconnected/activities/MapActivity.java
@@ -106,7 +106,7 @@ public class MapActivity extends BaseActivity {
     }
     
     private MyOwnItemizedOverlay getBusStops(){
-    	RESTRequest rR = new RESTRequest("http://145.37.88.226/yii/sites/BusStops/api/busstop");
+    	RESTRequest rR = new RESTRequest("http://145.37.92.124//yii/sites/BusStops/api/busstop");
     	//IGeoPoint point = mapView.getMapCenter();
     	rR.putDouble("gps_longitude", locator.getLongitude());
     	rR.putDouble("gps_latitude", locator.getLatitude());
