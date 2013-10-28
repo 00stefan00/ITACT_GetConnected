b93ab8b292ed7461fba6f07156dfb2c853fc642a
Jelle Wezel, van <jelle.van.wezel@gmail.com>
2013-10-28 14:27:25 +0100

    details

--- src/com/app/getconnected/activities/TransportDetailsActivity.java
+++ src/com/app/getconnected/activities/TransportDetailsActivity.java
@@ -8,7 +8,10 @@ import com.app.getconnected.R;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +26,7 @@ public class TransportDetailsActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_transport_details);
-		initLayout(R.string.title_activity_transport_result, true, true, true, false); //todo doe eens string maken
+		initLayout(R.string.title_activity_transport_result, true, true, true, true);
 		String json = getIntent().getExtras().getString("json");
 		try {
 			JSONObject jObject;
@@ -47,34 +50,27 @@ public class TransportDetailsActivity extends BaseActivity {
 	}
 	
 	private void initViews() throws JSONException{
-		RelativeLayout wrapper = (RelativeLayout)findViewById(R.id.transport_details_content_wrapper);
+		TableLayout wrapper = (TableLayout)findViewById(R.id.transport_details_content_wrapper);
 		for(int i = 0; i < legs.length() && i < 4; i++){
-			RelativeLayout view = (RelativeLayout) getLayoutInflater().inflate(R.layout.transport_details_view, wrapper, false);
-			initView(legs.getJSONObject(i),view);
-			System.out.println(((TextView)view.findViewById(R.id.transport_detail_title)).getText());
-			wrapper.addView(view);
+			TableRow row = (TableRow) getLayoutInflater().inflate(R.layout.transport_details_view, wrapper, false);
+			initView(legs.getJSONObject(i),row);
+			//System.out.println(((TextView)view.findViewById(R.id.transport_detail_title)).getText());
+			wrapper.addView(row);
 		}
 	}
 	
-	private void initView(JSONObject leg,RelativeLayout view)throws JSONException{
+	private void initView(JSONObject leg,TableRow view)throws JSONException{
 		String mode = leg.getString("mode");
 		String from = leg.getJSONObject("from").getString("name");
 		String departureTime = leg.getString("startTime");
 		String arivalTime = leg.getString("endTime");
 		String to = leg.getJSONObject("to").getString("name");
 		
-		TextView tv = (TextView)findViewById(R.id.dingvoordemo);
-		tv.append(mode +  "\n");
-		tv.append(departureTime + " -> " + from + "\n");
-		tv.append(arivalTime + " -> " + to + "\n\n");
-
-		
-		
-		//((TextView) view.findViewById(R.id.transport_detail_title)).setText(mode);
-		//((TextView) view.findViewById(R.id.transport_details_text_departure_time)).setText(departureTime);
-		//((TextView) view.findViewById(R.id.transport_details_text_departure_location)).setText(from);
-		//((TextView) view.findViewById(R.id.transport_details_text_arival_time)).setText(arivalTime);
-		//((TextView) view.findViewById(R.id.transport_details_text_arival_location)).setText(to);
+		((TextView) view.findViewById(R.id.transport_detail_title)).setText(mode);
+		((TextView) view.findViewById(R.id.transport_details_text_departure_time)).setText(departureTime);
+		((TextView) view.findViewById(R.id.transport_details_text_departure_location)).setText(from);
+		((TextView) view.findViewById(R.id.transport_details_text_arival_time)).setText(arivalTime);
+		((TextView) view.findViewById(R.id.transport_details_text_arival_location)).setText(to);
 		
 	}
 
