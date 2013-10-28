c729890acb1a49535662bf1ea20890d9171504fa
Stefan <stefan.strijker@outlook.com>
2013-10-28 12:54:40 +0100

    before show

--- src/com/app/getconnected/activities/MarketplaceActivity.java
+++ src/com/app/getconnected/activities/MarketplaceActivity.java
@@ -9,6 +9,7 @@ public class MarketplaceActivity extends BaseActivity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_marketplace);
 		initLayout(R.string.title_activity_marketplace, true, true, true, false);
