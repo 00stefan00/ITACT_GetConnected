f807d0669990d57ad77354d5c11b6c5b8499765e
Daninio <daniel.zijlstra@hotmail.com>
2013-10-31 10:17:13 +0100

    Test

--- src/com/app/getconnected/network/Config.java
+++ src/com/app/getconnected/network/Config.java
@@ -5,6 +5,6 @@ public class Config {
 	//public static String busStopAddress = "http://145.37.92.124/BusStops/api/busstop/";
 	public static String busStopAddress = "http://145.37.76.169/yii/sites/BusStops/api/busstop/";
 	
-	public static String tripPlannerAddress = "http://145.37.92.124:8081/opentripplanner-api-webapp/ws/plan";
+	public static String tripPlannerAddress = "http://145.37.86.94:8081/opentripplanner-api-webapp/ws/plan";
 	
 }
