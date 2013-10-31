d26465472891602c1fa631524b3cb749de8ab469
jorian99 <jorian_09@hotmail.com>
2013-10-31 13:56:31 +0100

    Added enter key press in LocationSelector

--- src/com/app/getconnected/activities/LocationSelectorActivity.java
+++ src/com/app/getconnected/activities/LocationSelectorActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -36,6 +38,19 @@ public class LocationSelectorActivity extends BaseActivity implements
 		autoCompView.setAdapter(new PlacesAutoCompleteAdapter(this,
 				R.layout.places_list_item));
 		autoCompView.setOnItemClickListener(this);
+		
+		autoCompView.setOnKeyListener(new OnKeyListener() {
+			@Override
+			public boolean onKey(View view, int keyCode, KeyEvent event) {
+		        if (event.getAction()!=KeyEvent.ACTION_DOWN)
+		            return false;
+		        if(keyCode == KeyEvent.KEYCODE_ENTER ){
+		            selectLocation(view, autoCompView.getText().toString());
+		            return true;
+		        }
+		        return false;
+			}
+		});
 
 		InputMethodManager keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 		keyboard.showSoftInput(autoCompView, 0);
