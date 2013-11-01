57ff48763628bfd88c0677e93c7e4b8d89ab34c3
jorian99 <jorian_09@hotmail.com>
2013-11-01 11:22:41 +0100

    New IP-address

--- src/com/app/getconnected/config/Config.java
+++ src/com/app/getconnected/config/Config.java
@@ -2,9 +2,9 @@ package com.app.getconnected.config;
 
 public class Config
 {
-	public static String busStopAddress = "http://145.37.86.94/BusStops/api/busstop/";
+	public static String busStopAddress = "http://145.37.92.55/BusStops/api/busstop/";
 	
-	public static String tripPlannerAddress = "http://145.37.86.94:8081/opentripplanner-api-webapp/ws/plan";
+	public static String tripPlannerAddress = "http://145.37.92.55:8081/opentripplanner-api-webapp/ws/plan";
 	
 	public static final boolean DEBUG = true;
 }
