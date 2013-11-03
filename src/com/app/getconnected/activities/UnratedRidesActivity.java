861fc14bcf08489dc5ea9a9e9346df5d430520bb
啊Denko啊 <juss4dabanta@gmail.com>
2013-11-03 14:29:20 +0100

    Continued rating implementation

--- src/com/app/getconnected/activities/UnratedRidesActivity.java
+++ src/com/app/getconnected/activities/UnratedRidesActivity.java
@@ -4,7 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -23,7 +26,7 @@ public class UnratedRidesActivity extends BaseActivity
 		initLayout(R.string.title_activity_unrated_rides, true, true, true,
 				false);
 		tableinit();
-		addTableRow("1371546903022", "r", "template_user", "m");
+		addTableRow("1371546903022", "r", "template_user", "m", "14002");
 	}
 	
 	private void tableinit() {
@@ -48,16 +51,16 @@ public class UnratedRidesActivity extends BaseActivity
         rowHeader.addView(endTime);
         rowHeader.addView(role);
         rowHeader.addView(name);
-        rowHeader.addView(gender);   
+        rowHeader.addView(gender);
         
         tl.addView(rowHeader, new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
         tl.setStretchAllColumns(true);		
 	}
 	
-	private void addTableRow(String endTimev, String rolev, String namev, String genderv){
+	private void addTableRow(String endTimev, String rolev, String namev, String genderv, String rideIdv){
 		long endTimeLong = Long.valueOf(endTimev);
 		Date endTimeDate = new java.util.Date(endTimeLong);
-		String endTimeString = new SimpleDateFormat("yyyy-MM-dd hh:mma", Locale.ENGLISH).format(endTimeDate);
+		String endTimeString = new SimpleDateFormat("yyyy-MM-dd hh:mm", Locale.ENGLISH).format(endTimeDate);
 		
 		TextView endTime = new TextView(this);
 		endTime.setText(endTimeString);
@@ -67,13 +70,26 @@ public class UnratedRidesActivity extends BaseActivity
         name.setText(namev);
         TextView gender = new TextView(this);
         gender.setText(genderv);
-        
+        Button buttonRate = new Button(this);
+        //TODO use strings.xml
+        buttonRate.setText("Rate");
+        buttonRate.setTag(rideIdv);
+        buttonRate.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	            // Perform action on click   
+	
+	            Intent intent = new Intent(UnratedRidesActivity.this, RateRideActivity.class);
+	            intent.putExtra("rideId", v.getTag().toString());
+	            startActivity(intent);
+	        }
+        });
         TableRow datarow = new TableRow(this);
 
         datarow.addView(endTime);
         datarow.addView(role);
         datarow.addView(name);
         datarow.addView(gender);
+        datarow.addView(buttonRate);
         
         tl.addView(datarow, new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 		
