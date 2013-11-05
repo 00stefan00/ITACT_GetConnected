fa18f863af61519153b60a9364867cd8c61f8730
Jelle Wezel, van <jelle.van.wezel@gmail.com>
2013-11-05 11:48:41 +0100

    ip addressss 112 deze keer :D

--- src/com/app/getconnected/config/Config.java
+++ src/com/app/getconnected/config/Config.java
@@ -2,9 +2,9 @@ package com.app.getconnected.config;
 
 public class Config
 {
-	public static String busStopAddress = "http://145.37.92.17/BusStops/api/busstop/";
+	public static String busStopAddress = "http://145.37.92.112/BusStops/api/busstop/";
 	
-	public static String tripPlannerAddress = "http://145.37.92.17:8081/opentripplanner-api-webapp/ws/plan";
+	public static String tripPlannerAddress = "http://145.37.92.112:8081/opentripplanner-api-webapp/ws/plan";
 	
 	public static final String OPEN_RIDE_API = "";
 	
